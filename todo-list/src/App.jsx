ab92bbe801f43d3052dcf4782aada9727711a5a9
praesidios <praesidios@gmail.com>
2019-05-20 23:25:31 +0300

    tastcommit

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { TaskList  } from './TaskList '
+import { TaskList  } from './TaskList ';
 
 export class App extends React.Component {
   render() {
